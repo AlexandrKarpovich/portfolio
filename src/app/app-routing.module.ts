9f196d5f2193b35e426d1cff50537460bda9bdab
Alex Karpovich <akarpovich@b2broker.net>
2022-04-21 15:33:16 +0300

    initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { MainComponent } from './components/main/main.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorksComponent } from './components/works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'main', component: MainComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'slills', component: SkillsComponent},
+  { path: 'works', component: WorksComponent},
+  { path: 'contacts', component: ContactsComponent},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { MainComponent } from './components/main/main.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    WorksComponent,
+    ContactsComponent,
+    MainComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
