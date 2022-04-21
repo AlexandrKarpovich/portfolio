3d0c4507fd2c47234918722d63cba87b8da30848
Alex Karpovich <akarpovich@b2broker.net>
2022-04-22 01:45:33 +0300

    add description new page

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit  {
+
+  title = 'Main';
 
   constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild('myCanvas')
+  private myCanvas: ElementRef = {} as ElementRef;
+
+  
+  ngAfterViewInit(): void {
+    
   }
+
+  
+
+
+
+
+  
 
 }
--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksComponent implements OnInit {
 
+  title = 'Works'
+
   constructor() { }
 
   ngOnInit(): void {
